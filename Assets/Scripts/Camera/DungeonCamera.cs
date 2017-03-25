bdef990b6f4074e7a112c2f78490952e62bcf665
s2haseeb <haseeb1496@gmail.com>
2017-03-25 15:47:26 -0400

    SkyCam: Added orthographic resizing capability for zooming
    This allows the skyCam to be controlled using the keys "M" and "N" to zoom in
    and out from the player respectively.

--- Assets/Scripts/Camera/DungeonCamera.cs
+++ Assets/Scripts/Camera/DungeonCamera.cs
@@ -10,13 +10,27 @@ public class DungeonCamera : MonoBehaviour {
 	{
 		offset = transform.position - target.transform.position;
 	}
-
+	
 	void LateUpdate() 
 	{
+		resizeOrthographicView();
+		
 		Vector3 desiredPosition = target.transform.position + offset;
 		Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
-
+		
 		this.transform.position = position;
 		this.transform.LookAt(target.transform);
+	}
+	
+	
+	void resizeOrthographicView()
+	{
+		if (Input.GetKey (KeyCode.M)) {
+			this.camera.orthographicSize /= 1.001f;
+		} else if (Input.GetKey (KeyCode.N)) {
+			this.camera.orthographicSize *= 1.001f;
+		} else {
+			this.camera.orthographicSize = 35.0f;
+		}
 	}
 }
